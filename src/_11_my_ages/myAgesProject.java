5affc173b947f94ec5d54524bba47022f73bce98
gabreb <62906858+gabreb@users.noreply.github.com>
2020-04-06 20:01:22 -0700

    hi

---
+++ src/_11_my_ages/myAgesProject.java
@@ -0,0 +1,10 @@
+package _11_my_ages;
+
+public class myAgesProject {
+public static void main(String[] args) {
+	// I am counting when I was zero
+	for (int i = 0; i < 14; i++) {
+		System.out.println(i);
+	}
+}
+}
