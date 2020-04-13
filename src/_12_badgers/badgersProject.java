40ac918bee88c9724c739d52b6626778b8fcaeb3
gabreb <62906858+gabreb@users.noreply.github.com>
2020-04-13 12:21:26 -0700

    hi

--- src/_12_badgers/badgersProject.java
+++ src/_12_badgers/badgersProject.java
@@ -2,26 +2,26 @@ package _12_badgers;
 
 public class badgersProject {
 	public static void main(String[] args) {
-		// said badger 12 times.
-		// mushroom 2 times.
-		// badger 12 times.
-		// mushroom 2 times;
-		// bager 12 times.
-		// ah snake ah snake. snake. ooh a snake
-		for (int x = 0; x < 2; x++) {
+		for (int z = 0; z > -1; z++) {
+		for (int x = 0; x < 3; x++) {
 			
 				for (int i = 0; i < 12; i++) {
-					System.out.println("badger");
+					System.out.print("badger, ");
 				}
+				System.out.println(); 
 			
-			
+			if(x<2) {
 			for (int m = 0; m < 2; m++) {
-				System.out.println("mushroom");
+				System.out.print("mushroom, ");
+			}
+			System.out.println();
 			}
 			
 		}
 		for (int x = 0; x < 4; x++) {
-			System.out.println("ooh a snake");
+			System.out.print(" a snake, ");
 		}
+		System.out.println();
 	}
+}
 }
